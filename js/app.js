5af97e77fe117cf02f2fa8d8e7ecb0aca5e64acb
Sameer Singh Bhandari <bhandarisameer512@gmail.com>
2024-01-31 12:13:24 +0530

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -40,3 +40,34 @@ for (let i = 0; i < tab.length; i++) {
 function goto(url)  {
     window.open(url, "_blank")
 }
+
+function DownloadResume() {
+    var fileName = "SameerSinghBhandariResume.pdf";
+    //Set the File URL.
+    var url = "../pdf/" + fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
